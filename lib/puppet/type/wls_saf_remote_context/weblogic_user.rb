6a732085709178c94a1ff190472e331711247b1e
Edwin Biemond <biemond@gmail.com>
2014-04-11 16:59:26 +0200

    remove default

--- lib/puppet/type/wls_saf_remote_context/weblogic_user.rb
+++ lib/puppet/type/wls_saf_remote_context/weblogic_user.rb
@@ -2,7 +2,6 @@ newproperty(:weblogic_user) do
   include EasyType
 
   desc "the weblogic user account "
-  defaultto 'weblogic'
 
   to_translate_to_resource do | raw_resource|
     raw_resource['weblogic_user']

835c3115c0b8ccd3821f6c309cb94035813d1853
Edwin Biemond <biemond@gmail.com>
2014-05-05 00:27:41 +0200

    rake fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,12 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'mocha'
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 # include common helpers
-support_path = File.expand_path(File.join(File.dirname(__FILE__), '..',
-                                          'spec/support/*.rb'))
+support_path = File.expand_path(File.join(File.dirname(__FILE__), '..','spec/support/*.rb'))
 Dir[support_path].each {|f| require f}
 
 RSpec.configure do |c|
   c.config = '/doesnotexist'
-  # c.manifest_dir = File.join(fixture_path, 'manifests')
   c.module_path  = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/modules'))
-  # Using an empty site.pp file to avoid: https://github.com/rodjek/rspec-puppet/issues/15
   c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
   c.mock_with :mocha
 end

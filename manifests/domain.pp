bc6a912b75ba84a5c9ca369dc3d74b1b32eed1bc
Edwin Biemond <biemond@gmail.com>
2014-05-14 21:38:41 +0200

    java_arguments hiera lookup changed to domain_java..

--- manifests/domain.pp
+++ manifests/domain.pp
@@ -15,7 +15,7 @@ define orawls::domain (
   $adminserver_name           = hiera('domain_adminserver'        , "AdminServer"),
   $adminserver_address        = hiera('domain_adminserver_address', undef),
   $adminserver_port           = hiera('domain_adminserver_port'   , 7001),
-  $java_arguments             = hiera('java_arguments', {}),               # java_arguments = { "ADM" => "...", "OSB" => "...", "SOA" => "...", "BAM" => "..."}
+  $java_arguments             = hiera('domain_java_arguments', {}),         # java_arguments = { "ADM" => "...", "OSB" => "...", "SOA" => "...", "BAM" => "..."}
   $nodemanager_address        = undef,
   $nodemanager_port           = hiera('domain_nodemanager_port'   , 5556),
   $weblogic_user              = hiera('wls_weblogic_user'         , "weblogic"),

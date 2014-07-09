77a9e141e4f9edc67c68f762893187bee32392e1
mkelderm <marc@vijfhuizen.com>
2014-07-09 15:16:16 +0200

    Added temporary-file-directory parameter
    Added temporary-file-directory parameter, when bsu_patch is applied. By
    default it is set to /tmp. You are now able to overule this by a
    parameter. This wil set the javo.io.tmpdir

--- lib/puppet/provider/bsu_patch/bsu_patch.rb
+++ lib/puppet/provider/bsu_patch/bsu_patch.rb
@@ -11,6 +11,7 @@ Puppet::Type.type(:bsu_patch).provide(:bsu_patch) do
     middleware_home_dir = resource[:middleware_home_dir]
     weblogic_home_dir   = resource[:weblogic_home_dir]
     patch_download_dir  = resource[:patch_download_dir]
+    temp_directory      = resource[:temp_directory]
 
     if action == :present
       bsuaction = "-install"
@@ -18,12 +19,18 @@ Puppet::Type.type(:bsu_patch).provide(:bsu_patch) do
       bsuaction = "-remove"
     end 
 
+    if temp_directory == nil
+      tmpdir = "/tmp"
+    else
+      tmpdir = temp_directory
+    end
+    
     Puppet.debug "bsu_patch action: #{action}"
 
     if patch_download_dir == nil
-      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh "+bsuaction+" -patchlist="+patchName+" -prod_dir="+weblogic_home_dir+" -verbose"
+      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh "+bsuaction+" -patchlist="+patchName+" -prod_dir="+weblogic_home_dir+" -verbose"+" -Djava.io.tmpdir="+tmpdir
     else 
-      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh "+bsuaction+" -patchlist="+patchName+" -prod_dir="+weblogic_home_dir+" -patch_download_dir="+patch_download_dir+" -verbose"
+      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh "+bsuaction+" -patchlist="+patchName+" -prod_dir="+weblogic_home_dir+" -patch_download_dir="+patch_download_dir+" -verbose"+" -Djava.io.tmpdir="+tmpdir
     end 
     #environment = ["USER="+user, "HOME=/home/"+user, "LOGNAME="+user]
     Puppet.debug "bsu_patch action: #{action} with command #{command}"
@@ -56,10 +63,16 @@ Puppet::Type.type(:bsu_patch).provide(:bsu_patch) do
     jdk_home_dir        = resource[:jdk_home_dir]
     patch_download_dir  = resource[:patch_download_dir]
 
+    if temp_directory == nil
+      tmpdir = "/tmp"
+    else
+      tmpdir = temp_directory
+    end
+    
     if patch_download_dir == nil
-      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh -view -status=applied -prod_dir="+weblogic_home_dir+" -verbose"
+      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh -view -status=applied -prod_dir="+weblogic_home_dir+" -verbose"+" -Djava.io.tmpdir="+tmpdir
     else 
-      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh -view -status=applied -prod_dir="+weblogic_home_dir+" -patch_download_dir="+patch_download_dir+" -verbose"
+      command = "cd "+middleware_home_dir+"/utils/bsu;"+middleware_home_dir+"/utils/bsu/bsu.sh -view -status=applied -prod_dir="+weblogic_home_dir+" -patch_download_dir="+patch_download_dir+" -verbose"+" -Djava.io.tmpdir="+tmpdir
     end 
 
     Puppet.debug "bsu_status for patch #{patchName} command: #{command}"

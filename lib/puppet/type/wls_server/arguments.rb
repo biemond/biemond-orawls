77a9fc6f5b0a06cd5a4afbb41a7cc8ff99f0be97
biemond <biemond@gmail.com>
2014-09-16 17:12:31 +0200

    server arguments fix

--- lib/puppet/type/wls_server/arguments.rb
+++ lib/puppet/type/wls_server/arguments.rb
@@ -2,13 +2,13 @@ newproperty(:arguments) do
   include EasyType
 
   desc 'The server arguments of the server'
-
+  
   to_translate_to_resource do | raw_resource|
     raw_resource['arguments']
   end
 
   def should=(values)
-    values = values.flatten.join('\n') if values.is_a?(Array)
+    values = values.flatten.join("\n") if values.is_a?(Array)
     @should = values
   end
 

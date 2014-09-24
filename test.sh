891a2567bf61ee46b161173202bd44f7563ce25d
unknown <ebiemond@EBIEMOND-LAP.oradev.oraclecorp.com>
2014-09-24 20:00:19 +0200

    unit test on windows desktop

--- test.sh
+++ test.sh
@@ -26,3 +26,21 @@ bundle exec rake syntax
 bundle exec rake lint
 bundle exec rake ci:setup:rspec spec
 #bundle exec rake spec
+
+
+# for windows
+install ruby 1.9.3 for windows
+install puppet 3.7.1 msi (https://downloads.puppetlabs.com/windows/)
+
+gem install bundler --no-rdoc --no-ri
+bundle install --without development
+gem update --system 2.1.11
+
+set PUPPET_VERSION=3.7.1
+bundle update
+SET PATH="C:\Program Files (x86)\Puppet Labs\Puppet\bin";"C:\Program Files (x86)\Puppet Labs\Puppet\facter\bin";"C:\Program Files (x86)\Puppet Labs\Puppet\hiera\bin";"C:\Program Files (x86)\Puppet Labs\Puppet\bin";"C:\Program Files (x86)\Puppet Labs\Puppet\sys\tools\bin";%PATH%
+SET RUBYLIB="C:\Program Files (x86)\Puppet Labs\Puppet\lib";"C:\Program Files (x86)\Puppet Labs\Puppet\facter\lib";"C:\Program Files (x86)\Puppet Labs\Puppet\hiera\lib";%RUBYLIB%;
+
+bundle exec rake syntax
+bundle exec rake lint
+bundle exec rake ci:setup:rspec spec

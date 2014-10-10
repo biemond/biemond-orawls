78311f964f58938cc47105e6ae76e78e1354e0a8
bradmcnaughton <bradamcnaughton@gmail.com>
2014-10-10 13:56:38 +1030

    Add "Order" param to wls_authentication_provider
    WLS_Authentication_Provider type can read in an optional "order"
    parameter to set the ordering of authentication providers.

---
+++ lib/puppet/type/wls_authentication_provider/order.rb
@@ -0,0 +1,12 @@
+newproperty(:order) do
+  include EasyType
+
+  desc 'The order of the Authentication Provider (0 is highest)'
+
+  defaultto ''
+
+  to_translate_to_resource do | raw_resource|
+    raw_resource['order']
+  end
+
+end

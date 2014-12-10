e35b0bbcf05119e95ee9e4d1a1c0a873c751a11d
bradmcnaughton <bradamcnaughton@gmail.com>
2014-12-10 15:08:13 +1030

    Add Secure Replication Parameter to wls_cluster
    Add Secure Replication Parameter to wls_cluster

---
+++ lib/puppet/type/wls_cluster/securereplication.rb
@@ -0,0 +1,14 @@
+newproperty(:securereplication) do
+  include EasyType
+  include EasyType::Validators::Name
+
+  desc 'Enable Secure Replication for the cluster'
+  newvalues(1, 0)
+
+  defaultto '0'
+
+  to_translate_to_resource do | raw_resource|
+    raw_resource['securereplication']
+  end
+
+end

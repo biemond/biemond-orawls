ff20800f858914477f269460bd5b291178727c14
Edwin Biemond <biemond@gmail.com>
2014-12-16 20:59:55 +0100

    removed default value of secure replication on wls_cluster

--- lib/puppet/type/wls_cluster/securereplication.rb
+++ lib/puppet/type/wls_cluster/securereplication.rb
@@ -5,8 +5,6 @@ newproperty(:securereplication) do
   desc 'Enable Secure Replication for the cluster'
   newvalues(1, 0)
 
-  defaultto '0'
-
   to_translate_to_resource do | raw_resource|
     raw_resource['securereplication']
   end

b4b969695b0ffd74d4bb77e7362158badd0f9d2d
Edwin Biemond <biemond@gmail.com>
2015-02-24 12:18:35 +0100

    rspec fixes for bsu

--- spec/defines/bsu_spec.rb
+++ spec/defines/bsu_spec.rb
@@ -21,29 +21,29 @@ describe 'orawls::bsu', :type => :define do
                    :osfamily        => 'RedHat' }}
 
     describe "cache dir creation" do
-      it do 
+      it do
         should contain_file("/opt/oracle/middleware11gR1/utils/bsu/cache_dir")
       end
     end
 
     describe "download patch" do
-      it { 
+      it {
            should contain_file("/install/p17572726_1036_Generic.zip").with({
              'source'  => 'puppet:///middleware/p17572726_1036_Generic.zip',
-           }).that_comes_before('Exec[extract p17572726_1036_Generic.zip]')  
-         }  
+           }).that_comes_before('Exec[extract p17572726_1036_Generic.zip]')
+         }
     end
 
     describe "extract patch" do
-      it { 
+      it {
            should contain_exec("extract p17572726_1036_Generic.zip").with({
-             'command'  => 'unzip -n /install/p17572726_1036_Generic.zip -d /opt/oracle/middleware11gR1/utils/bsu/cache_dir',
-           }).that_comes_before('Bsu_patch[FCX7]')  
-         }  
+             'command'  => 'unzip -o /install/p17572726_1036_Generic.zip -d /opt/oracle/middleware11gR1/utils/bsu/cache_dir',
+           }).that_comes_before('Bsu_patch[FCX7]')
+         }
     end
 
     describe "install BSU" do
-      it { 
+      it {
            should contain_bsu_patch("FCX7").with({
              'ensure'               => 'present',
              'middleware_home_dir'  => '/opt/oracle/middleware11gR1',
@@ -51,7 +51,7 @@ describe 'orawls::bsu', :type => :define do
              'weblogic_home_dir'    => '/opt/oracle/middleware11gR1/wlserver_103',
              'jdk_home_dir'         => '/usr/java/jdk1.7.0_45',
            })
-        }  
+        }
     end
   end
 
@@ -74,21 +74,21 @@ describe 'orawls::bsu', :type => :define do
                    :osfamily        => 'RedHat' }}
 
     describe "cache dir creation" do
-      it do 
+      it do
         should contain_file("/opt/oracle/middleware11gR1/utils/bsu/cache_dir")
       end
     end
 
     describe "extract patch" do
-      it { 
+      it {
            should contain_exec("extract p17572726_1036_Generic.zip").with({
-             'command'  => 'unzip -n /mnt/p17572726_1036_Generic.zip -d /opt/oracle/middleware11gR1/utils/bsu/cache_dir',
-           }).that_comes_before('Bsu_patch[FCX7]')  
-         }  
+             'command'  => 'unzip -o /mnt/p17572726_1036_Generic.zip -d /opt/oracle/middleware11gR1/utils/bsu/cache_dir',
+           }).that_comes_before('Bsu_patch[FCX7]')
+         }
     end
 
     describe "install BSU" do
-      it { 
+      it {
            should contain_bsu_patch("FCX7").with({
              'ensure'               => 'present',
              'middleware_home_dir'  => '/opt/oracle/middleware11gR1',
@@ -96,7 +96,7 @@ describe 'orawls::bsu', :type => :define do
              'weblogic_home_dir'    => '/opt/oracle/middleware11gR1/wlserver_103',
              'jdk_home_dir'         => '/usr/java/jdk1.7.0_45',
            })
-        }  
+        }
     end
   end
 

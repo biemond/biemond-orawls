1dfb8592f3f3d970a2f35d03ec7370dece228971
Edwin Biemond <biemond@gmail.com>
2015-03-11 21:03:32 +0100

    store enabled on wls_jmsserver

--- lib/puppet/type/wls_jmsserver/store_enabled.rb
+++ lib/puppet/type/wls_jmsserver/store_enabled.rb
@@ -1,13 +1,12 @@
 newproperty(:store_enabled) do
   include EasyType
 
-  desc 'Specifies whether message persistence is supported for this JMS server.'
+  desc 'Specifies whether message persistence is supported for this JMS server. only for wls 10.3'
 
-  newvalues('0','1')
+  newvalues('0', '1')
 
   to_translate_to_resource do | raw_resource|
     raw_resource['store_enabled']
   end
-
 
 end

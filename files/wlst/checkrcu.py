1fcd77730d7284f6d99aa9f71ffa52d379ca3ae6
m2127299045 <m2127299045@gmail.com>
2015-04-22 06:37:54 -0700

    If you type lower case letters in the Schema User Name field, the Upgrade Assistant converts the name to upper case as per https://docs.oracle.com/middleware/1213/core/UAREF/ua_screens.htm#UAREF1050

--- files/wlst/checkrcu.py
+++ files/wlst/checkrcu.py
@@ -8,9 +8,8 @@ sysuser  = sys.argv[4]
 conn = DriverManager.getConnection(jdbcurl, sysuser + " as sysdba", password)
 stmt = conn.createStatement()
 try:
-    rs = stmt.executeQuery("select distinct 'found' from system.schema_version_registry where mrc_name ='"+prefix+"'")
+    rs = stmt.executeQuery("select distinct 'found' from system.schema_version_registry where upper(mrc_name) = upper('"+prefix+"')")
 
-    emp = {}
     while rs.next():
       print rs.getString(1)
 

c929dd3f128fa9dbedbf47dc71edb6cc2bf89061
matejicek <ondrej.matejicek@embedit.cz>
2015-04-24 15:08:34 +0200

    fix weblogic_plugin_enabled bugs

--- lib/puppet/type/wls_server/weblogic_plugin_enabled.rb
+++ lib/puppet/type/wls_server/weblogic_plugin_enabled.rb
@@ -2,7 +2,7 @@ newproperty(:weblogic_plugin_enabled) do
   include EasyType
 
   desc 'The Weblogic Plugin Enabled on the server'
-  newvalues('1', '0', '-1')
+  newvalues('1', '0')
 
   to_translate_to_resource do | raw_resource|
     raw_resource['weblogic_plugin_enabled']

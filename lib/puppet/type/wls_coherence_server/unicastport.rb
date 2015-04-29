587927976546da31067e2a3dd6770cae025dd21a
Nan Liu <nan.liu@gmail.com>
2015-04-29 12:13:46 -0700

    Fix coherence server machine and add documentation.
    * Coherence machine was hardcoded to localmachine by mistake.
    * Add some basic documentation.

--- lib/puppet/type/wls_coherence_server/unicastport.rb
+++ lib/puppet/type/wls_coherence_server/unicastport.rb
@@ -2,7 +2,7 @@ newproperty(:unicastport) do
   include EasyType
   include EasyType::Validators::Name
 
-  desc 'The uni cast port of this cluster'
+  desc 'The unicast port of this cluster'
 
   to_translate_to_resource do | raw_resource|
     raw_resource['unicastport']

c4cb828d1fbbd89b35b238a2026b57953ec2924c
rob.houghton <rob.houghton@cdl.co.uk>
2015-05-13 16:36:23 +0100

    Removed default forwarddelay attribute value

--- lib/puppet/type/wls_jms_queue/forwarddelay.rb
+++ lib/puppet/type/wls_jms_queue/forwarddelay.rb
@@ -2,8 +2,6 @@ newproperty(:forwarddelay) do
   include EasyType
   include EasyType::Mungers::Integer
 
-  defaultto(-1)
-
   desc 'The number of seconds after which a uniform distributed queue member with no consumers will wait before forwarding its messages to other uniform distributed queue members that do have consumers. '
 
   to_translate_to_resource do | raw_resource|

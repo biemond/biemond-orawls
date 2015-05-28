e9faf68d3394a6465e50d8b425b87efd4729ed37
Nan Liu <nan.liu@gmail.com>
2015-05-28 08:20:00 -0700

    Redact password.

--- lib/puppet/type/wls_setting/weblogic_password.rb
+++ lib/puppet/type/wls_setting/weblogic_password.rb
@@ -6,4 +6,20 @@ newproperty(:weblogic_password) do
   to_translate_to_resource do |raw_resource|
     raw_resource[self.name]
   end
+
+  def change_to_s(currentvalue, newvalue)
+    if currentvalue == :absent
+      "created password"
+    else
+      "changed password"
+    end
+  end
+
+  def is_to_s(currentvalue)
+    '[old password redacted]'
+  end
+
+  def should_to_s(newvalue)
+    '[new password redacted]'
+  end
 end

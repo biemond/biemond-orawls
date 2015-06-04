2d284c52698a2a01e71aa8e8bc4da9d3024a639e
Robert Mestrum <robert.mestrum@cgi.com>
2015-06-04 10:23:45 +0200

    Added security to wls_server_channel type

--- lib/puppet/type/wls_server_channel.rb
+++ lib/puppet/type/wls_server_channel.rb
@@ -43,6 +43,7 @@ module Puppet
     parameter :server
     parameter :channel_name
     parameter :timeout
+    parameter :custom_identity_privatekey_passphrase
 
     property :protocol
     property :enabled
@@ -54,6 +55,10 @@ module Puppet
     property :outboundenabled
     property :tunnelingenabled
     property :max_message_size
+    property :custom_identity_alias
+    property :two_way_ssl
+    property :client_certificate_enforced
+    property :channel_identity_customized
 
     add_title_attributes(:server, :channel_name) do
       /^((.*\/)?(.*):(.*)?)$/

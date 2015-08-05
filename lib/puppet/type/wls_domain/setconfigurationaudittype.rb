c8f18d69b0703ad73e04b0eed9a78929c1a3f4a0
Edwin Biemond <biemond@gmail.com>
2015-08-05 20:40:58 +0200

    removed a default else it will lead to a change

--- lib/puppet/type/wls_domain/setconfigurationaudittype.rb
+++ lib/puppet/type/wls_domain/setconfigurationaudittype.rb
@@ -2,7 +2,6 @@ newproperty(:setconfigurationaudittype) do
   include EasyType
 
   desc 'The configuration audit type'
-  defaultto 'None'
 
   to_translate_to_resource do | raw_resource|
     raw_resource['setconfigurationaudittype']

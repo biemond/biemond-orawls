3dd878ade4968760faa969726adce247ea1b16a6
Bert Hajee <bert.hajee@enterprisemodules.com>
2016-01-05 12:21:14 +0100

    [wls_datasource] Removed support for the encrypted password

--- lib/puppet/type/wls_datasource/password.rb
+++ lib/puppet/type/wls_datasource/password.rb
@@ -8,29 +8,7 @@ newproperty(:password) do
   desc <<-EOD
     The datasource password.
 
-    There are two ways you can use this property:
-
-    1. with an encrypted value
-    2. with a clear text password
-
-    Usage with a encrypted password:
-
-        wls_datasource { ....:
-          ...
-          password => '{AES}yAf7w4hKUOa3XSUsPCPb1LWnwy0PSNiq7+HAahXXWQY=',
-          ...
-        }
-
-    Puppet treats everything that starts with `{...}`, where the dots are uppercase letters,
-    as an encrypted value. These values are passed directly to WebLogic and stored in the domain.
-
-    A method to get the encrypted value would be:
-
-    1. Set the password through the WebLogic management interface
-    2. User `puppet resource wls_datasource datasource_name` to get all information including the 
-       encrypted password.
-
-    Usage with a clear text password:
+    Usage :
 
         wls_datasource { ....:
           ...
@@ -38,24 +16,12 @@ newproperty(:password) do
           ...
         }
 
-    Clear text passwords are passed to WebLogic for encryption and then stored in the database.
+    The password string is passed to WebLogic for encryption and then stored in the domain.
 
   EOD
 
   def insync?(is)
-    # If the specified password in the manifest is encrypted, we compare it
-    # with the fetched password (which is also encrypted). 
-    #
-    # If the password in the puppet manifest is a plan text password, Puppet asks 
-    # Weblogic to decrypt the current datasource password stored in the domain and 
-    # return this to puppet for comparison. The Puppet code does not store the decrypted password
-    # on disk.
-    #
-    if encrypted_password?
-      is == should
-    else
-      wls_decrypt_value(is, resource) == should
-    end
+    current_password == should
   end
 
   def change_to_s(currentvalue, newvalue)
@@ -75,16 +41,18 @@ newproperty(:password) do
   end
 
   to_translate_to_resource do | raw_resource|
-    raw_resource['password']
+    '<encrypted password>'
   end
 
-  private
+  def current_password
     #
-    # If the password starts with {AES}, {2DES} or an other combination if capitals and numbers 
-    # between curly braces, we treat the password as an encrypted value.
+    # Puppet asks Weblogic to decrypt the current datasource password stored in the domain and 
+    # return this to puppet for comparison. The Puppet code does not store the decrypted password
+    # on disk.
     #
-    def encrypted_password?
-      !should.scan(/\A\{[0-9A-Z]*\}.*\Z/).empty?
-    end
+    datasource_name = resource[:datasource_name]
+    key = "JDBCSystemResources/#{datasource_name}/JDBCResource/#{datasource_name}/JDBCDriverParams/#{datasource_name}/PasswordEncrypted"
+    wls_get_password(key, resource)
+  end
 
 end
--- lib/utils/wls_functions.rb
+++ lib/utils/wls_functions.rb
@@ -1,3 +1,5 @@
+require 'utils/indent'
+
 module Utils
   #
   # This module contains all WLS functions that can be directly called from ruby. These
@@ -6,22 +8,25 @@ module Utils
   #
   module WlsFunctions
 
-    ##
-    #
-    # decrypt the specified value. 
-    #
-    def wls_decrypt_value(value, resource)
-      wls_function('wls_decrypt_value', binding)
-    end
+    wls_functions = [
+      :wls_get_value,       # get the value at a specified key
+      :wls_get_password,    # get the decrypted password string at a specified key. 
+    ]
 
+    wls_functions.each do | method_name|
+      define_method(method_name) do |value, resource|
+        wls_function(method_name, binding)
+      end
+    end
 
 private
 
-    def wls_function(function_name, caller_binding)
-      eval("function_name = '#{function_name}'", caller_binding)  # Add functio name to binding
+    def wls_function(method_name, caller_binding)
       environment = { 'action' => 'execute', 'type' => :wls_functions }
-      value = wlst template("puppet:///modules/orawls/functions/#{function_name}.py.erb", caller_binding), environment
-      value.first[function_name]
+      function_content = template("puppet:///modules/orawls/functions/#{method_name}.py.erb", caller_binding)
+      function_content = function_content.indent(4)
+      value = wlst template("puppet:///modules/orawls/functions/function_base.py.erb", binding), environment
+      value.first['value']
     end
 
   end

0182f4d3961552836dd24e6c03c099aee8047ea6
Edwin Biemond <biemond@gmail.com>
2016-07-16 01:15:52 +0200

    lint fix

--- manifests/domain.pp
+++ manifests/domain.pp
@@ -601,13 +601,13 @@ define orawls::domain (
     # remove nodemanager properties so we can later add custom trust parameters in nodemanager.p
     if ( $custom_trust == true and ($version == 1212 or $version == 1213 or $version >= 1211 )) {
       exec { "rm ${domain_name} nodemanager.properties":
-        command     => "rm -rf ${$domain_dir}/nodemanager/nodemanager.properties",
-        cwd         => $download_dir,
-        timeout     => 0,
-        path        => $exec_path,
-        user        => $os_user,
-        group       => $os_group,
-        require     => Exec["execwlst ${domain_name} ${title}"],
+        command => "rm -rf ${$domain_dir}/nodemanager/nodemanager.properties",
+        cwd     => $download_dir,
+        timeout => 0,
+        path    => $exec_path,
+        user    => $os_user,
+        group   => $os_group,
+        require => Exec["execwlst ${domain_name} ${title}"],
       }
     }
 

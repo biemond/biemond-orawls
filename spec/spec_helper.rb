b5944dbb0cc74a4b8445905d8b4bb9f7d242d2c4
Bryan Belanger <bbelanger@azcender.com>
2016-07-25 12:02:50 -0400

    Add testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,22 @@
-begin
-  require 'coveralls'
-  Coveralls.wear!
-
-rescue LoadError
-  puts "No Coveralls support"
-end
-
-require 'rspec-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'pathname'
+require 'rspec-puppet-facts'
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-# include common helpers
-support_path = File.expand_path(File.join(File.dirname(__FILE__), '..','spec/support/*.rb'))
-Dir[support_path].each {|f| require f}
+include RspecPuppetFacts
+
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.start do
+  add_filter '/spec'
+  add_filter '/vendor'
+  formatter SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::Console
+    ]
+  )
+end
 
 RSpec.configure do |c|
-  c.config = '/doesnotexist'
-  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
 end
-
-dir = Pathname.new(__FILE__).parent
-Puppet[:modulepath] = File.join(dir, 'fixtures', 'modules')
-Puppet[:libdir] = "#{Puppet[:modulepath]}/easy_type/lib"
-
-def param_value(subject, type, title, param)
-  subject.resource(type, title).send(:parameters)[param.to_sym]
-end
-
-#at_exit { RSpec::Puppet::Coverage.report! }

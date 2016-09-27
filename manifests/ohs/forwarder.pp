1c987bd6e392d26b7a994a44d6d720d7a355a483
Thiago David - DATAPREVRN <thiago.david@dataprev.gov.br>
2016-09-27 15:24:50 -0300

    Changing file path parameter (now using domain_path)

--- manifests/ohs/forwarder.pp
+++ manifests/ohs/forwarder.pp
@@ -6,16 +6,16 @@ define orawls::ohs::forwarder (
   $owner,
   $group,
   $servers,
-  $ohs_config_dir,
-  $ensure = 'present',
-  $location = $title,
+  $domain_path,
   $default_port = 7001,
+  $ensure       = 'present',
+  $location     = $title,
 ) {
   # TODO: create and use function sanitize_string (fmw.pp, duplicated code)
   $convert_spaces_to_underscores = regsubst($title,'\s','_','G')
   $sanitised_title = regsubst($convert_spaces_to_underscores,'[^a-zA-Z0-9_-]','','G')
 
-  file { "${ohs_config_dir}/mod_wl_ohs.d/${sanitised_title}.conf":
+  file { "${domain_path}/config/fmwconfig/components/OHS/ohs1/mod_wl_ohs.d/${sanitised_title}.conf":
     ensure  => $ensure,
     content => template('orawls/ohs/forwarder.conf.erb'),
     owner   => $owner,

968f53e810292146a6cdec07d8787e90814a6156
Justin White <Justin.White@sjrb.ca>
2017-06-28 13:13:12 -0600

    Adding to pinnedToThread to index.py script

--- lib/puppet/type/wls_datasource/pinnedtothread.rb
+++ lib/puppet/type/wls_datasource/pinnedtothread.rb
@@ -2,9 +2,9 @@ newproperty(:pinnedtothread) do
   include EasyType
 
   desc 'Flag to keep a pooled database connection even after the application closes the logical connection'
-  newvalues(true, false)
+  newvalues(1, 0)
 
-  defaultto 'false'
+  defaultto '0'
 
   to_translate_to_resource do | raw_resource|
     raw_resource['pinnedtothread']
